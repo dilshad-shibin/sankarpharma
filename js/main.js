b0fdddee9ff3f0641e98d3a7f788a00d60fa3ed9
Dilshad Shibin <49745082+dilshad-shibin@users.noreply.github.com>
2020-11-13 12:33:21 +0530

    contents correct tiling and responsive changes

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@
 		loop: true,
 		margin: -1,
 		items: 1,
-		nav: true,
+		nav: false,
 		navText: ['<i class="ion-ios-arrow-back" aria-hidden="true"></i>', '<i class="ion-ios-arrow-forward" aria-hidden="true"></i>'],
 		autoplay: true,
 		autoplayTimeout: 3000,
